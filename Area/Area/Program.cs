e14525b93bf158253b5128dafd999a850274c5c3
krgirard33 <ken.girard@gmail.com>
2017-02-27 19:41:57 -0600

    Created Area, which figures the area of a circle given a radius

---
+++ Area/Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The radius of a circle with a radius of " + radius + " is: " + Math.PI * (radius * radius));
+            Console.ReadLine();
+        }
+    }
+}
