9827ab88d78b99be787accfbf8e4f03a33de6588
krgirard33 <ken.girard@gmail.com>
2017-03-11 16:44:48 -0600

    Area, Hello World, Hello Methods, List

--- Area/Area/Program.cs
+++ Area/Area/Program.cs
@@ -10,17 +10,16 @@ namespace Area
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a radius: ");
-            double radius = Convert.ToDouble(Console.ReadLine());
-            if (radius < 0)
-                {
+            do {
+                Console.WriteLine("Enter a radius: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Positive numbers only");
                 Console.ReadLine();
-            }
-            else
-            {
                 Console.WriteLine("The radius of a circle with a radius of " + radius + " is: " + Math.PI * (radius * radius));
                 Console.ReadLine();
+            }
+            while {
+                radius < 0;
             }
         }
     }
---
+++ List/List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int didgit = 0;
+            int sum = 0;
+            var numbers = new List<int>() { 8, 3, 2, 4, 13, 14, 15, 16, 7, 18, 19, 20 };
+            for (didgit in numbers);
+            {
+                if (int didgit = 0; didgit % 2 == 0 )
+                {
+                    sum += didgit;
+                }
+            }
+
+        }
+    }
+}
