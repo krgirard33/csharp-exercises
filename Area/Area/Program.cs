e35c4c5b3ac61ddcf00b9498999570b7d8fee6b8
krgirard33 <ken.girard@gmail.com>
2017-03-13 00:13:46 -0500

    This is older

--- Area/Area/Program.cs
+++ Area/Area/Program.cs
@@ -10,18 +10,15 @@ namespace Area
     {
         static void Main(string[] args)
         {
+            do 
             Console.WriteLine("Enter a radius: ");
             double radius = Convert.ToDouble(Console.ReadLine());
-            if (radius < 0)
-                {
+            
                 Console.WriteLine("Positive numbers only");
                 Console.ReadLine();
-            }
-            else
-            {
                 Console.WriteLine("The radius of a circle with a radius of " + radius + " is: " + Math.PI * (radius * radius));
                 Console.ReadLine();
-            }
-        }
+           
+        } while (radius< 0)
     }
 }
